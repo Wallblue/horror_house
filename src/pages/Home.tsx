e2903a28a74ad165263df792ab5ad05d259683cf
Wallblue <nrb.busquet@gmail.com>
2025-07-24 21:42:40 +0200

    Dynamised home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,9 @@
+import { useEffect, useState } from "react";
 import HeaderList, {HeaderElement} from "../components/HeaderList"
+import { API_DOMAIN } from "../const";
+import { useErrorHandler } from "../hooks/useErrorHandler";
+import { PaginatedResponse, Room } from "../mocks/types";
+import LoadingSpinner from "../components/LoadingSpinner";
 
   const sessionsInfos : HeaderElement[] = [
     {header:'Le Manoir Hanté', text:'Explorez un manoir ancien où hantent des esprits et secrets terrifiants. Préparez-vous à résoudre des énigmes dans un décor gothique.'},
@@ -7,12 +12,52 @@ import HeaderList, {HeaderElement} from "../components/HeaderList"
   ];
 
 export default function Home() {
+  const {executeWithErrorHandling} = useErrorHandler();
+  const [sessions, setSessions] = useState<PaginatedResponse<Room>>();
+
+  const fetchSessions = async () => {
+    executeWithErrorHandling(
+      async () => {
+        const res = await fetch(API_DOMAIN + "/rooms?limit=10");
+        if (!res.ok) {
+          throw new Error(`Erreur ${res.status}: ${res.statusText}`);
+        }
+        const roomsData: PaginatedResponse<Room> = await res.json();
+        return roomsData;
+      },
+      (sessionsData) => {
+        setSessions(sessionsData);
+      }
+    );
+  };
+
+  useEffect(() => {
+    fetchSessions()
+  }, []);
+
   return (
     <>
       <h1>La Maison Horrifique</h1>
       <section>
-        <h2>Nos sessions d’escape game</h2>
-        <HeaderList elements={sessionsInfos} />
+        {sessions === undefined ? (
+          <LoadingSpinner
+            message="Chargement des utilisateurs..."
+            size="large"
+          />
+        ) : (
+          <>
+            {sessions.data.length > 0 && (
+              <>
+                <h2>Nos sessions d’escape game</h2>
+                <HeaderList elements={sessions.data.map(s => ({
+                  header: s.name,
+                  text: s.description,
+                }))} />
+              </>
+            )}
+          </>
+        )}
+        
       </section>
       <section>
         <h2>À propos de l’entreprise</h2>
